58a6074b02dd9c6b0ed9916d5921423629b4535f
Allen Garvey <allen.garvey@gmail.com>
2018-05-09 11:37:14 -0400

    Add basic JavaScript for carousel

--- inc/templates/home/home.php
+++ inc/templates/home/home.php
@@ -1,11 +1,11 @@
 <div class="home-header">
     <div class="carousel">
-        <img src="<?= IMAGES_URL.'nyc.png'; ?>" alt=""/>
+        <img src="<?= IMAGES_URL.'nyc.png'; ?>" alt="" id="carousel-image"/>
         <div class="carousel-button-container carousel-left">
-            <div class="carousel-button"><span class="carousel-button-text">&lt;</span></div>
+            <div class="carousel-button" id="carousel-button-left"><span class="carousel-button-text">&lt;</span></div>
         </div>
         <div class="carousel-button-container carousel-right">
-            <div class="carousel-button"><span class="carousel-button-text">&gt;</span></div>
+            <div class="carousel-button" id="carousel-button-right"><span class="carousel-button-text">&gt;</span></div>
         </div>
     </div>
     <div class="site-tagline">
@@ -16,3 +16,31 @@
         <div class="tagline-overlay"></div>
     </div>
 </div>
+<script>
+    (function(){
+        var carouselImage = document.getElementById('carousel-image');
+        var carouselButtonRight = document.getElementById('carousel-button-right');
+        var carouselButtonLeft = document.getElementById('carousel-button-left');
+        var images = ['nyc.png', 'home-sweet.png', 'tomato.png', 'neon-flower.png', 'jackson.png'];
+        var currentIndex = 0;
+
+        function displayImage(){
+            carouselImage.src = '/images/' + images[currentIndex];
+        }
+
+        carouselButtonLeft.onclick = function(){
+            currentIndex--;
+            if(currentIndex < 0){
+                currentIndex = images.length - 1;
+            }
+            displayImage();
+        };
+        carouselButtonRight.onclick = function(){
+            currentIndex++;
+            if(currentIndex >= images.length){
+                currentIndex = 0;
+            }
+            displayImage();
+        };
+    })();
+</script>
